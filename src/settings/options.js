2378162aee6e6b606f4a2689306b455e3476b7c4
Tiago Silva <tfn.da.silva@gmail.com>
2024-05-04 16:07:22 +0100

    feat: save option on change

--- src/settings/options.js
+++ src/settings/options.js
@@ -1,30 +1,24 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
-    const color = document.getElementById('color').value;
-  
-    chrome.storage.sync.set(
-      { diffColor: color },
-      () => {
-        // Update status to let user know options were saved.
-        const status = document.getElementById('status');
-        status.textContent = 'Options saved. Please refresh page.';
-        setTimeout(() => {
-          status.textContent = '';
-        }, 1500);
-      }
-    );
-  };
-  
-  // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
-  const restoreOptions = () => {
-    chrome.storage.sync.get(
-      { diffColor: '#AAFF00'},
-      (items) => {
-        document.getElementById('color').value = items.diffColor;
-      }
-    );
-  };
-  
-  document.addEventListener('DOMContentLoaded', restoreOptions);
-  document.getElementById('save').addEventListener('click', saveOptions);
+  const color = document.getElementById("color").value;
+
+  chrome.storage.sync.set({ diffColor: color }, () => {
+    // Update status to let user know options were saved.
+    const status = document.getElementById("status");
+    status.textContent = "Options saved. Please refresh page.";
+    setTimeout(() => {
+      status.textContent = "";
+    }, 1500);
+  });
+};
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+const restoreOptions = () => {
+  chrome.storage.sync.get({ diffColor: "#AAFF00" }, (items) => {
+    document.getElementById("color").value = items.diffColor;
+  });
+};
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.getElementById("color").addEventListener("change", saveOptions, false);
